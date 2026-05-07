1f728dc370bcca6106fab4786847c4b8aa21ed39
unplugged-maksim <maksim@unplugged.com>
2026-05-07 11:55:47 +0300

    Bump account-helper dep to 1.1.3

--- up-antivirus-common/build.gradle.kts
+++ up-antivirus-common/build.gradle.kts
@@ -55,9 +55,9 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.picasso:picasso:2.71828")
 
-    "developmentApi"("com.unplugged:account-helper-development:1.1.2")
-    "stagingApi"("com.unplugged:account-helper-staging:1.1.2")
-    "productionApi"("com.unplugged:account-helper-production:1.1.2")
+    "developmentApi"("com.unplugged:account-helper-development:1.1.3")
+    "stagingApi"("com.unplugged:account-helper-staging:1.1.3")
+    "productionApi"("com.unplugged:account-helper-production:1.1.3")
 
     implementation ("dnsjava:dnsjava:3.5.0")
 
